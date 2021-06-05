eeedff3a31422f47e7f0da0210ca45fb6b7ba540
miriamdev <getre4dy@gmnail.com>
2021-06-05 06:46:37 -0300

    version con vistas html

---
+++ controllers/bicicleta.js
@@ -0,0 +1,42 @@
+var Bicicleta = require('../models/bicicleta');
+
+
+//bicis:Bicicleta.allbicis, bicis es un objeto que referencia a la lista de bicicletas, en el layout index.pug se lo llama para listar con una tabla las bicicletas en la ruta bicicletas/index.pug
+exports.bicicleta_list = function(req,res){
+    res.render('bicicletas/index',{bicis: Bicicleta.allBicis});
+}
+
+exports.bicicleta_create_get = function(req,res){
+    res.render('bicicletas/create');
+}
+
+exports.bicicleta_create_post = function(req,res){
+   var bici = new Bicicleta(req.body.id, req.body.color, req.body.modelo);
+   bici.ubicacion = [req.body.lat, req.body.lng];
+   Bicicleta.add(bici);
+
+   res.redirect('/bicicletas');
+}
+
+exports.bicicleta_update_get = function(req,res){
+    var bici = Bicicleta.findById(req.param.id);
+    console.log(bici);
+
+    res.render('bicicletas/update', {bici});
+}
+
+exports.bicicleta_update_post = function(req,res){
+   var bici = Bicicleta.findById(req.param.id);
+   bici.id = req.body.id;
+   bici.color = req.body.color;
+   bici.modelo = req.body.modelo;
+   bici.ubicacion = [req.body.lat, req.body.lng];
+   
+   res.redirect('/bicicletas');
+}
+
+exports.bicicleta_delete_post = function(req,res){
+    Bicicleta.removeById(req.body.id);
+
+    res.redirect('/bicicletas');
+}
---
+++ models/bicicleta.js
@@ -0,0 +1,50 @@
+var Bicicleta = function(id, color, modelo, ubicacion){
+    this.id = id;
+    this.color = color;
+    this.modelo = modelo;
+    this.ubicacion = ubicacion;
+}
+
+//Redefino toString
+Bicicleta.prototype.toString = function(){
+    return 'id:' + this.id + "| color:" + this.color;
+}
+
+
+//Array para guardar las bicis
+Bicicleta.allBicis = [];
+
+Bicicleta.add = function(unaBici){
+    Bicicleta.allBicis.push(unaBici)
+}
+
+let a = new Bicicleta(1,'rojo', 'urbana',[51.504201, -0.082483]);
+let b = new Bicicleta(2,'blanca', 'urbana',[51.50332, -0.082998]);
+
+Bicicleta.add(a);
+Bicicleta.add(b);
+
+//Buscar una bici por id
+Bicicleta.findById = function(unaBiciId){
+   var unaBici = Bicicleta.allBicis.find(x => x.id == unaBiciId);
+    if(unaBici)
+        return unaBici;
+    else
+        throw new Error(`No existe una bicicleta con ese id ${unaBici}`
+        );
+    
+}
+
+//Eliminar una bici 
+Bicicleta.removeById = function(unaBiciId){
+    // var unabici = Bicicleta.findById(unaBiciId);
+    for(let i=0; i<Bicicleta.allBicis.length; i++){
+        if(Bicicleta.allBicis[i].id == unaBiciId){
+            Bicicleta.allBicis.slice(i,1);
+            break;
+        }
+    }
+}
+
+
+module.exports = Bicicleta;
